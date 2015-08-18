13fde830906f06601498e888d94f69fe664c81a4
djaus2 <davidjones@sportronics.com.au>
2015-08-18 23:44:57 +1000

    App now has mores filters
    List of device types to menu that can be sued for filter

--- DeviceWatcher/DeviceWatcher/MainPage.xaml.cs
+++ DeviceWatcher/DeviceWatcher/MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Media.Imaging;
 
 using Windows.Devices.Enumeration;
 using Windows.Devices.Enumeration.Pnp;
-
+using System.Collections.ObjectModel;
 
 namespace DeviceWatcher
 {
@@ -28,11 +28,25 @@ namespace DeviceWatcher
     /// 
     public sealed partial class MainPage : Page
     {
+        public class clsDevType
+        {
+            public string TypeOfDevice { get; set; }
+
+
+            public clsDevType(string typeOfDevice)
+            {
+                TypeOfDevice = typeOfDevice;
+            }
+        }
 
         public MainPage()
         {
 
             this.InitializeComponent();
+            //DevTypes.Add(new clsDevType("jjkl"));
+            //DevTypes.Add(new clsDevType("ajjkl"));
+            //DevTypes.Add(new clsDevType("bjjkl"));
+            //cbDevType.DataContext = DevTypes;
         }
 
         Windows.UI.Core.CoreDispatcher dispatcher;
@@ -42,6 +56,10 @@ namespace DeviceWatcher
         public static List<DeviceInformation> MyInterfaces = new List<DeviceInformation>();
         public static bool isEnumerationComplete = false;
         public static string StopStatus = null;
+
+        //public static List<clsDevType> DevTypes = new List<clsDevType>();
+        public static List<string> DevTypes = new List<string>();
+        //public ObservableCollection<clsDevType> DevTypes = new ObservableCollection<clsDevType>();
 
         async void WatchDevices_Click(object sender, RoutedEventArgs eventArgs)
         {
@@ -204,19 +222,35 @@ namespace DeviceWatcher
         /// </summary>
         async void DisplayDeviceInterfaceArray()
         {
+            //Get currently selected device type to be compared to first part of Id
+            string devType = "";
+            if (cbDevType.SelectedIndex != -1)
+            {
+                devType = (string)cbDevType.SelectedItem;
+            }
             //Get devices as list and filter
             var myInterfaces1 = interfaces.ToList<DeviceInformation>();
-            var myInterfaces2 = from p in myInterfaces1 where filter(p) select p;
+            var myInterfaces2 = from p in myInterfaces1 where filter(p, devType) select p;
 
             //Apply set by name if selected
             if (chkSortOnName.IsChecked == true)
             {
-                 myInterfaces2 = from p in myInterfaces1 where filter(p) orderby p.Name select p ;
+                 myInterfaces2 = from p in myInterfaces1 where filter(p,devType) orderby p.Name select p ;
             }
 
             //Copy to the list used XAML list control and refresh
             MyInterfaces = myInterfaces2.ToList<DeviceInformation>();
             DeviceInterfacesOutputList.DataContext = MyInterfaces;
+            tbCount.Text = MyInterfaces.Count.ToString();
+
+            //Update list of devTypes
+            var dt = from d in myInterfaces1 where d != null select getDevType(d.Id);
+            //getDevType() can return nulls for if id name is a Guid
+            var dt2 = from d2 in dt where d2 != null select d2;
+            var dt3 = (from d3 in dt2 select d3.TypeOfDevice).Distinct<string>();
+            DevTypes = dt3.ToList<string>();
+            DevTypes= DevTypes.OrderBy(q => q).ToList();
+            cbDevType.DataContext = DevTypes;
 
             //Used when XAML list was created manually:
             //DeviceInterfacesOutputList.Items.Clear();
@@ -231,12 +265,22 @@ namespace DeviceWatcher
             //}
         }
 
+        clsDevType getDevType(string id)
+        {
+            id = id.Substring(0, 12);
+            string[] items = id.Split(new char[] { '#' });
+            string item = items[0].Replace("\\\\?\\", "");
+            if (item.Contains("{"))
+                return null;
+            return new clsDevType(items[0].Replace("\\\\?\\", ""));
+        }
+
         /// <summary>
         /// Apply filters for Linq query
         /// </summary>
         /// <param name="deviceInterface"></param>
         /// <returns>true if interface passes filter tests</returns>
-        bool filter(DeviceInformation deviceInterface)
+        bool filter(DeviceInformation deviceInterface, string devType)
         {
             if (deviceInterface == null)
                 return false;
@@ -254,6 +298,19 @@ namespace DeviceWatcher
             {
                 if (!deviceInterface.Name.ToUpper().Contains("USB"))
                     return false;
+            }
+            else
+            {
+                if (devType != "")
+                {
+                    if (cbSelect.IsChecked == true)
+                    { 
+                        string[] items = deviceInterface.Id.Split(new char[] { '#' });
+                        string _devType = items[0].Replace("\\\\?\\", "");
+                        if (_devType != devType)
+                            return false;
+                    }
+                }
             }
             if (tbFilter.Text != "")
             {
@@ -383,6 +440,11 @@ namespace DeviceWatcher
             }
 
             tblLatest.Text +="\n" + id;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //buttonDropDown.IsOpen = true;
         }
     }
 }
